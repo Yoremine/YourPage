a26a52bc8cf170c4b13dd00d386c2d011aafb41c
Yoremine <144691124+Yoremine@users.noreply.github.com>
2023-11-23 20:35:25 +0300

    1

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+let profileName = document.querySelector('#profile__input_name');
+console.log(`Значение p1 = ${profileName.placeholder}`);
+
+let profileAbout = document.querySelector(`#profile__input_About`);
+console.log(`Значение p1 = ${profile__input_About.placeholder}`);
+
+let profileButton = document.querySelector('.profile__add');
+
+console.log(`Значение button = ${profileButton}`);
+
+function showClick() {
+    console.log(`Имя = ${profileName.value}\nО себе = ${profileAbout.value}`);
+}
+
+profileButton.addEventListener('click', showClick);
+
+let profilePhoto = document.querySelector('.profile__photo');
+
+function showPhoto() {
+   let profilePhoto = document.querySelector('#profilePhoto');
+   profilePhoto.addEventListener('click', profilePhoto);
+   if (profilePhoto.style.display == "none"){
+        profilePhoto.style.display = "block";
+    } else {
+        profilePhoto.style.display = "none";
+    }
+}
